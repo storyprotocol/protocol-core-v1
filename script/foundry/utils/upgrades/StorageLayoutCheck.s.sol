7b5e80ddae312aa7d7f6b583a62e301b17d3e334
Raul <raul.mf86@gmail.com>
2024-03-23 20:37:07 -0300

    storage layout checker scripts

--- script/foundry/utils/upgrades/StorageLayoutCheck.s.sol
+++ script/foundry/utils/upgrades/StorageLayoutCheck.s.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.23;
 import { Script } from "forge-std/Script.sol";
 import { Vm } from "forge-std/Vm.sol";
 import { console } from "forge-std/console.sol";
+import { strings } from "solidity-stringutils/src/strings.sol";
 
+import { Utils } from "@openzeppelin-foundry-upgrades/src/internal/Utils.sol";
 import { StringUtil } from "../StringUtil.sol";
 
 /// @title StorageLayoutChecker
@@ -15,7 +17,10 @@ import { StringUtil } from "../StringUtil.sol";
 /// MUST be called in scripts that deploy or upgrade contracts
 /// MUST be called with `--ffi` flag
 contract StorageLayoutChecker is Script {
-    function run() public {
+
+    using strings for *;
+
+    function run() virtual public {
         _validate();
     }
 
@@ -24,7 +29,7 @@ contract StorageLayoutChecker is Script {
     /// instead of going 1 by 1 using ffi.
     function _validate() private {
         string[] memory inputs = _buildValidateCommand();
-        Vm.FfiResult memory result = _runAsBashCommand(inputs);
+        Vm.FfiResult memory result = Utils.runAsBashCommand(inputs);
         string memory stdout = string(result.stdout);
 
         // CLI validate command uses exit code to indicate if the validation passed or failed.
@@ -40,44 +45,6 @@ contract StorageLayoutChecker is Script {
         }
     }
 
-    /**
-     * @dev Runs an arbitrary command using bash.
-     * @param inputs Inputs for a command, e.g. ["grep", "-rl", "0x1234", "out/build-info"]
-     * @return The result of the corresponding bash command as a Vm.FfiResult struct
-     */
-    function _runAsBashCommand(string[] memory inputs) internal returns (Vm.FfiResult memory) {
-        string[] memory bashCommand = _toBashCommand(inputs, "bash");
-        Vm.FfiResult memory result = vm.tryFfi(bashCommand);
-        if (result.exitCode != 0 && result.stdout.length == 0 && result.stderr.length == 0) {
-            // On Windows, using the bash executable from WSL leads to a non-zero exit code and no output
-            revert(StringUtil.concat('Failed to run bash command with "', bashCommand[0]));
-        } else {
-            return result;
-        }
-    }
-
-    /**
-     * @dev Converts an array of inputs to a bash command.
-     * @param inputs Inputs for a command, e.g. ["grep", "-rl", "0x1234", "out/build-info"]
-     * @param bashPath Path to the bash executable or just "bash" if it is in the PATH
-     * @return A bash command that runs the given inputs, e.g. ["bash", "-c", "grep -rl 0x1234 out/build-info"]
-     */
-    function _toBashCommand(string[] memory inputs, string memory bashPath) internal pure returns (string[] memory) {
-        string memory commandString;
-        for (uint i = 0; i < inputs.length; i++) {
-            commandString = string.concat(commandString, inputs[i]);
-            if (i != inputs.length - 1) {
-                commandString = string.concat(commandString, " ");
-            }
-        }
-
-        string[] memory result = new string[](3);
-        result[0] = bashPath;
-        result[1] = "-c";
-        result[2] = commandString;
-        return result;
-    }
-
     function _buildValidateCommand() private view returns (string[] memory) {
         string memory outDir = "out";
 
@@ -89,8 +56,6 @@ contract StorageLayoutChecker is Script {
         inputBuilder[i++] = string.concat("@openzeppelin/upgrades-core");
         inputBuilder[i++] = "validate";
         inputBuilder[i++] = string.concat(outDir, "/build-info");
-
-        inputBuilder[i++] = "--requireReference";
 
         // Create a copy of inputs but with the correct length
         string[] memory inputs = new string[](i);

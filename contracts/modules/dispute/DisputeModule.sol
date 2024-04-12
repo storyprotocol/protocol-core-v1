b4a5bc62e3e75964015dbada36fd2e9beccdfbc5
Spablob <tiagonunespereira@gmail.com>
2024-04-13 00:29:49 +0300

    fix comment size

--- contracts/modules/dispute/DisputeModule.sol
+++ contracts/modules/dispute/DisputeModule.sol
@@ -275,7 +275,7 @@ contract DisputeModule is
         if (parentDispute.targetIpId != parentIpId) revert Errors.DisputeModule__ParentIpIdMismatch();
 
         // a dispute current tag prior to being resolved can be in 3 states - IN_DISPUTE, 0, or a tag (ie. "PLAGIARISM)
-        // by restricting IN_DISPUTE and 0 - we ensure that the parent has been tagged as infringing before resolving dispute
+        // by restricting IN_DISPUTE and 0 - it is ensire the parent has been tagged before resolving dispute
         if (parentDispute.currentTag == IN_DISPUTE || parentDispute.currentTag == bytes32(0))
             revert Errors.DisputeModule__ParentNotTagged();
 

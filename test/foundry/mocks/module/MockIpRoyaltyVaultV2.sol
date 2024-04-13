e666ef3d055c9034413ea70050b9bf95f9b60e1f
Raul <raul.mf86@gmail.com>
2024-04-13 19:56:56 -0300

    lint

--- test/foundry/mocks/module/MockIpRoyaltyVaultV2.sol
+++ test/foundry/mocks/module/MockIpRoyaltyVaultV2.sol
@@ -4,7 +4,6 @@ import { IpRoyaltyVault } from "contracts/modules/royalty/policies/IpRoyaltyVaul
 pragma solidity 0.8.23;
 
 contract MockIpRoyaltyVaultV2 is IpRoyaltyVault {
-
     /// @dev Storage structure for the MockIPRoyaltyVaultV2
     /// @custom:storage-location erc7201:story-protocol.MockIPRoyaltyVaultV2
     struct MockIPRoyaltyVaultV2Storage {
@@ -12,7 +11,8 @@ contract MockIpRoyaltyVaultV2 is IpRoyaltyVault {
     }
 
     // keccak256(abi.encode(uint256(keccak256("story-protocol.MockIPRoyaltyVaultV2")) - 1)) & ~bytes32(uint256(0xff));
-    bytes32 private constant MockIPRoyaltyVaultV2StorageLocation = 0x2942176f94974e015a9b06f79a3a2280d18f1872591c134ba237fa184e378300;
+    bytes32 private constant MockIPRoyaltyVaultV2StorageLocation =
+        0x2942176f94974e015a9b06f79a3a2280d18f1872591c134ba237fa184e378300;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor(address royaltyPolicyLAP, address disputeModule) IpRoyaltyVault(royaltyPolicyLAP, disputeModule) {
@@ -33,5 +33,4 @@ contract MockIpRoyaltyVaultV2 is IpRoyaltyVault {
             $.slot := MockIPRoyaltyVaultV2StorageLocation
         }
     }
-        
 }

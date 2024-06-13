138e4186c9f14b9454f08046874fa35f2b0bea42
Raul <raul.mf86@gmail.com>
2024-06-13 18:04:57 -0300

    fix

--- test/foundry/upgrades/UpgradesV1_1_0.t.sol
+++ test/foundry/upgrades/UpgradesV1_1_0.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// pragma solidity 0.8.23;
+pragma solidity 0.8.23;
 
 // import { ProtocolAdmin } from "contracts/lib/ProtocolAdmin.sol";
 // import { RoyaltyPolicyLAP } from "contracts/modules/royalty/policies/RoyaltyPolicyLAP.sol";

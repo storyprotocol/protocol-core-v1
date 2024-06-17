afd64e01f17a34070ec1c973dc6a10e45195d054
Kingster <atarneo2015@gmail.com>
2024-06-17 13:10:18 +0800

    Introduce IPA Grouping

---
+++ contracts/interfaces/modules/grouping/IGroupingModule.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.23;
+
+import { IModule } from "../base/IModule.sol";
+
+/// @title IGroupingModule
+/// @notice This interface defines the entry point for users to manage group in the Story Protocol.
+/// It defines the workflow of grouping actions and coordinates among all grouping components.
+/// The Grouping Module is responsible for adding ip to group, removing ip from group and claiming reward.
+interface IGroupingModule is IModule {
+    /// @notice Emitted when added ip to group.
+    /// @param groupId The address of the group.
+    /// @param ipId The IP ID.
+    event AddedIpToGroup(
+        address indexed groupId,
+        address indexed ipId
+    );
+
+    /// @notice Emitted when removed ip from group.
+    /// @param groupId The address of the group.
+    /// @param ipId The IP ID.
+    event RemovedIpFromGroup(
+        address indexed groupId,
+        address indexed ipId
+    );
+
+    /// @notice Emitted when claimed reward.
+    /// @param groupId The address of the group.
+    /// @param token The address of the token.
+    /// @param ipId The IP ID.
+    /// @param amount The amount of reward.
+    event ClaimedReward(
+        address indexed groupId,
+        address indexed token,
+        address indexed ipId,
+        uint256 amount
+    );
+
+    /// @notice Adds IP to group.
+    /// the function must be called by the Group IP owner or an authorized operator.
+    /// @param groupIpId The address of the group IP.
+    /// @param ipIds The IP IDs.
+    function addIp(address groupIpId, address[] calldata ipIds) external;
+
+    /// @notice Removes IP from group.
+    /// the function must be called by the Group IP owner or an authorized operator.
+    /// @param groupIpId The address of the group IP.
+    /// @param ipIds The IP IDs.
+    function removeIp(address groupIpId, address[] calldata ipIds) external;
+
+    /// @notice Claims reward.
+    /// @param groupId The address of the group.
+    /// @param token The address of the token.
+    /// @param ipIds The IP IDs.
+    function claimReward(address groupId, address token, address[] calldata ipIds) external;
+}
---
+++ contracts/interfaces/registries/IGroupIPAssetRegistry.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.23;
+
+
+/// @title Interface for Group IPA  Registry
+/// @notice This interface manages the registration and tracking of Group IPA
+interface IGroupIPAssetRegistry {
+
+    /// @notice Emits when a Group IPA is officially registered into the protocol.
+    /// @param ipId The address of the registered Group IPA.
+    /// @param chainId The chain identifier of where the Group NFT resides.
+    /// @param tokenContract The token contract address of the Group NFT.
+    /// @param tokenId The token ID of the Group NFT.
+    event IPGroupRegistered(
+        address ipId,
+        uint256 indexed chainId,
+        address indexed tokenContract,
+        uint256 indexed tokenId
+    );
+
+
+    /// @notice Registers a Group IPA
+    /// @param groupPolicy The address of the group policy
+    /// @return groupId The address of the newly registered Group IPA.
+    function registerGroup(address groupPolicy) external returns (address groupId);
+
+    /// @notice Checks whether a group IPA was registered based on its ID.
+    /// @param groupId The address of the Group IPA.
+    /// @return isRegistered Whether the Group IPA was registered into the protocol.
+    function isGroupRegistered(address groupId) external view returns (bool);
+
+    /// @notice Retrieves the group policy for a Group IPA
+    /// @param groupId The address of the Group IPA.
+    /// @return groupPolicy The address of the group policy.
+    function getGroupPolicy(address groupId) external view returns (address);
+
+    /// @notice Retrieves the group members for a Group IPA
+    /// @param groupId The address of the Group IPA.
+    /// @param startIndex The start index of the group members to retrieve
+    /// @param size The size of the group members to retrieve
+    /// @return groupMembers The addresses of the group members
+    function getGroupMembers(address groupId, uint256 startIndex, uint256 size) external view returns (address[] memory);
+
+    /// @notice Checks whether an IP is a member of a Group IPA
+    /// @param groupId The address of the Group IPA.
+    /// @param ipId The address of the IP.
+    /// @return isMember Whether the IP is a member of the Group IPA.
+    function containsIp(address groupId, address ipId) external view returns(bool);
+
+    /// @notice Retrieves the total number of members in a Group IPA
+    /// @param groupId The address of the Group IPA.
+    /// @return totalMembers The total number of members in the Group IPA.
+    function totalMembers(address groupId) external view returns (uint256);
+}

6b6145a9c2936c4f31926281495769f96cd3bb0d
Kingster <atarneo2015@gmail.com>
2024-08-14 10:29:52 -0700

    fix lint

--- contracts/GroupNFT.sol
+++ contracts/GroupNFT.sol
@@ -14,126 +14,121 @@ import { Errors } from "./lib/Errors.sol";
 
 /// @title GroupNFT
 abstract contract GroupNFT is IGroupNFT, ERC721Upgradeable, AccessManagedUpgradeable, UUPSUpgradeable {
-        using Strings for *;
+    using Strings for *;
 
-        /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
-        IIPAssetRegistry public immutable IP_ASSET_REGISTRY;
+    /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
+    IIPAssetRegistry public immutable IP_ASSET_REGISTRY;
 
-        /// @notice Emitted for metadata updates, per EIP-4906
-        event BatchMetadataUpdate(uint256 _fromTokenId, uint256 _toTokenId);
+    /// @notice Emitted for metadata updates, per EIP-4906
+    event BatchMetadataUpdate(uint256 _fromTokenId, uint256 _toTokenId);
 
-        /// @dev Storage structure for the GroupNFT
-        /// @custom:storage-location erc7201:story-protocol.GroupNFT
-        struct GroupNFTStorage {
-            string imageUrl;
-            uint256 totalSupply;
+    /// @dev Storage structure for the GroupNFT
+    /// @custom:storage-location erc7201:story-protocol.GroupNFT
+    struct GroupNFTStorage {
+        string imageUrl;
+        uint256 totalSupply;
+    }
+
+    // keccak256(abi.encode(uint256(keccak256("story-protocol.GroupNFT")) - 1)) & ~bytes32(uint256(0xff));
+    bytes32 private constant GroupNFTStorageLocation =
+        0x62a0d75e37bea0c3e666dc72a74112fc6af15ce635719127e380d8ca1e555d00;
+
+    modifier onlyIPAssetRegistry() {
+        if (msg.sender != address(IP_ASSET_REGISTRY)) {
+            revert Errors.GroupNFT__CallerNotIPAssetRegistry(msg.sender);
         }
+        _;
+    }
 
-        // keccak256(abi.encode(uint256(keccak256("story-protocol.GroupNFT")) - 1)) & ~bytes32(uint256(0xff));
-        bytes32 private constant GroupNFTStorageLocation =
-            0x62a0d75e37bea0c3e666dc72a74112fc6af15ce635719127e380d8ca1e555d00;
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor(address iPAssetRegistry) {
+        IP_ASSET_REGISTRY = IIPAssetRegistry(iPAssetRegistry);
+        _disableInitializers();
+    }
 
-        modifier onlyIPAssetRegistry() {
-            if (msg.sender != address(IP_ASSET_REGISTRY)) {
-                revert Errors.GroupNFT__CallerNotIPAssetRegistry(msg.sender);
-            }
-            _;
+    /// @dev Initializes the GroupNFT contract
+    function initialize(address accessManager, string memory imageUrl) public initializer {
+        if (accessManager == address(0)) {
+            revert Errors.GroupNFT__ZeroAccessManager();
         }
+        __ERC721_init("Programmable IP Group IP NFT", "GroupNFT");
+        __AccessManaged_init(accessManager);
+        __UUPSUpgradeable_init();
+        _getGroupNFTStorage().imageUrl = imageUrl;
+    }
 
-        /// @custom:oz-upgrades-unsafe-allow constructor
-        constructor(address iPAssetRegistry) {
-            IP_ASSET_REGISTRY = IIPAssetRegistry(iPAssetRegistry);
-            _disableInitializers();
+    /// @dev Sets the Licensing Image URL.
+    /// @dev Enforced to be only callable by the protocol admin
+    /// @param url The URL of the Licensing Image
+    function setLicensingImageUrl(string calldata url) external restricted {
+        GroupNFTStorage storage $ = _getGroupNFTStorage();
+        $.imageUrl = url;
+        emit BatchMetadataUpdate(0, $.totalSupply);
+    }
+
+    /// @notice Mints a Group NFT.
+    /// @param minter The address of the minter.
+    /// @param receiver The address of the receiver of the minted Group NFT.
+    /// @return groupNftId The ID of the minted Group NFT.
+    function mintGroupNFT(address minter, address receiver) external onlyIPAssetRegistry returns (uint256 groupNftId) {
+        GroupNFTStorage storage $ = _getGroupNFTStorage();
+        groupNftId = $.totalSupply++;
+        _mint(receiver, groupNftId);
+        emit GroupNFTMinted(minter, receiver, groupNftId);
+    }
+
+    /// @notice Returns the total number of minted group IPA NFT since beginning,
+    /// @return The total number of minted group IPA NFT.
+    function totalSupply() external view returns (uint256) {
+        return _getGroupNFTStorage().totalSupply;
+    }
+
+    /// @notice ERC721 OpenSea metadata JSON representation of Group IPA NFT
+    function tokenURI(
+        uint256 id
+    ) public view virtual override(ERC721Upgradeable, IERC721Metadata) returns (string memory) {
+        GroupNFTStorage storage $ = _getGroupNFTStorage();
+
+        /* solhint-disable */
+        // Follows the OpenSea standard for JSON metadata
+
+        // base json, open the attributes array
+        string memory json = string(
+            abi.encodePacked(
+                "{",
+                '"name": "Story Protocol IP Assets Group #',
+                id.toString(),
+                '",',
+                '"description": IPAsset Group",',
+                '"external_url": "https://protocol.storyprotocol.xyz/ipa/',
+                id.toString(),
+                '",',
+                '"image": "',
+                $.imageUrl,
+                '"'
+            )
+        );
+
+        // close the attributes array and the json metadata object
+        json = string(abi.encodePacked(json, "}"));
+
+        /* solhint-enable */
+
+        return string(abi.encodePacked("data:application/json;base64,", Base64.encode(bytes(json))));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    //                         Upgrades related                               //
+    ////////////////////////////////////////////////////////////////////////////
+
+    /// @dev Returns the storage struct of GroupNFT.
+    function _getGroupNFTStorage() private pure returns (GroupNFTStorage storage $) {
+        assembly {
+            $.slot := GroupNFTStorageLocation
         }
+    }
 
-        /// @dev Initializes the GroupNFT contract
-        function initialize(address accessManager, string memory imageUrl) public initializer {
-            if (accessManager == address(0)) {
-                revert Errors.GroupNFT__ZeroAccessManager();
-            }
-            __ERC721_init("Programmable IP Group IP NFT", "GroupNFT");
-            __AccessManaged_init(accessManager);
-            __UUPSUpgradeable_init();
-            _getGroupNFTStorage().imageUrl = imageUrl;
-        }
-
-        /// @dev Sets the Licensing Image URL.
-        /// @dev Enforced to be only callable by the protocol admin
-        /// @param url The URL of the Licensing Image
-        function setLicensingImageUrl(string calldata url) external restricted {
-            GroupNFTStorage storage $ = _getGroupNFTStorage();
-            $.imageUrl = url;
-            emit BatchMetadataUpdate(0, $.totalSupply);
-        }
-
-        /// @notice Mints a Group NFT.
-        /// @param minter The address of the minter.
-        /// @param receiver The address of the receiver of the minted Group NFT.
-        /// @return groupNftId The ID of the minted Group NFT.
-        function mintGroupNFT(
-            address minter,
-            address receiver
-        ) external onlyIPAssetRegistry returns (uint256 groupNftId) {
-            GroupNFTStorage storage $ = _getGroupNFTStorage();
-            groupNftId = $.totalSupply++;
-            _mint(receiver, groupNftId);
-            emit GroupNFTMinted(minter, receiver, groupNftId);
-        }
-
-
-        /// @notice Returns the total number of minted group IPA NFT since beginning,
-        /// @return The total number of minted group IPA NFT.
-        function totalSupply() external view returns (uint256) {
-            return _getGroupNFTStorage().totalSupply;
-        }
-
-
-        /// @notice ERC721 OpenSea metadata JSON representation of Group IPA NFT
-        function tokenURI(
-            uint256 id
-        ) public view virtual override(ERC721Upgradeable, IERC721Metadata) returns (string memory) {
-            GroupNFTStorage storage $ = _getGroupNFTStorage();
-
-            /* solhint-disable */
-            // Follows the OpenSea standard for JSON metadata
-
-            // base json, open the attributes array
-            string memory json = string(
-                abi.encodePacked(
-                    "{",
-                    '"name": "Story Protocol IP Assets Group #',
-                    id.toString(),
-                    '",',
-                    '"description": IPAsset Group",',
-                    '"external_url": "https://protocol.storyprotocol.xyz/ipa/',
-                    id.toString(),
-                    '",',
-                    '"image": "',
-                    $.imageUrl,
-                    '"'
-                )
-            );
-
-            // close the attributes array and the json metadata object
-            json = string(abi.encodePacked(json, "}"));
-
-            /* solhint-enable */
-
-            return string(abi.encodePacked("data:application/json;base64,", Base64.encode(bytes(json))));
-        }
-
-        ////////////////////////////////////////////////////////////////////////////
-        //                         Upgrades related                               //
-        ////////////////////////////////////////////////////////////////////////////
-
-        /// @dev Returns the storage struct of GroupNFT.
-        function _getGroupNFTStorage() private pure returns (GroupNFTStorage storage $) {
-            assembly {
-                $.slot := GroupNFTStorageLocation
-            }
-        }
-
-        /// @dev Hook to authorize the upgrade according to UUPSUpgradeable
-        /// @param newImplementation The address of the new implementation
-        function _authorizeUpgrade(address newImplementation) internal override restricted {}
+    /// @dev Hook to authorize the upgrade according to UUPSUpgradeable
+    /// @param newImplementation The address of the new implementation
+    function _authorizeUpgrade(address newImplementation) internal override restricted {}
 }

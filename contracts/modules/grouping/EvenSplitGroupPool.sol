e273e67f1232043b200327d20a4d804a39b10817
Kingster <atarneo2015@gmail.com>
2024-09-27 15:51:35 -0700

    remove unused imports

--- contracts/modules/grouping/EvenSplitGroupPool.sol
+++ contracts/modules/grouping/EvenSplitGroupPool.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.26;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-//import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 import { IGroupRewardPool } from "../../interfaces/modules/grouping/IGroupRewardPool.sol";

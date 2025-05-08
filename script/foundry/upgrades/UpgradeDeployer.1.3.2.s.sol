78da80bdf73fa51250834ff1b3661965747b8bc5
Spablob <99089658+Spablob@users.noreply.github.com>
2025-05-08 23:37:29 +0100

    Update script/foundry/upgrades/UpgradeDeployer.1.3.2.s.sol
    Co-authored-by: Ramarti <raul.mf86@gmail.com>

--- script/foundry/upgrades/UpgradeDeployer.1.3.2.s.sol
+++ script/foundry/upgrades/UpgradeDeployer.1.3.2.s.sol
@@ -58,7 +58,7 @@ contract UpgradeDeployerExample is JsonDeploymentHandler, BroadcastManager, Upgr
     uint256 internal create3SaltSeed = CREATE3_DEFAULT_SEED;
 
     string constant PREV_VERSION = "v1.3.1";
-    string constant PROPOSAL_VERSION = "v1.3.2"; // e.g. v1.3.2
+    string constant PROPOSAL_VERSION = "v1.3.2";
 
     address accessController;
     address licensingModule;
